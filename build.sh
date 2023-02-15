f4571e5d5a3cf70756c2cd3b4b3e13b36e3ecb0e
sam <skasimalla@gmail.com>
2023-02-14 19:52:14 -0500

    Latest

--- build.sh
+++ build.sh
@@ -13,7 +13,9 @@ conan install . -s build_type=Debug --build=missing
 cmake ./CMakeLists.txt -G "Visual Studio 16 2019" -A "x64"
 cmake .
 cmake --build . --config Release
-#mkdir -p bin
-#cd bin
-#./Example
-#echo "Success"
+
+./bin/Example 
+
+conan remote add test-conan-local-dev  https://artifactory-unified.soleng-us.jfrog.team/artifactory/api/conan/test-conan-local-dev
+conan user -p AP9dFgyhMsyBASfQ6DgrN8GXoL3 -r test-conan-local-dev sam
+conan upload bin/Example -r test-conan-local-dev --all
