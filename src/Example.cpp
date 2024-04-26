bc57248c47c9cc19e3451b1c4ad92fcca44eb0f4
Sam Kasimalla <45541473+skasimalla@users.noreply.github.com>
2024-04-26 10:56:29 -0400

    Update Example.cpp

--- src/Example.cpp
+++ src/Example.cpp
@@ -1,23 +1,17 @@
-#include <iostream>
-#include <random>
-#include <ctime>
+#include<stdio.h>
 
-int main() {
-    
-    std::string haikus[3] = {
-        "Conan, frog of might,\nLeaps across the moonlit night,\nSword gleams in the fight.",
-        "In the swamp's deep heart,\nConan stands, set apart,\nBravery his art.",
-        "Frog warrior leaps,\nEnemy lines, he sweeps,\nHis legend, it creeps."
-    };
+using namespace std;
+
+int square(int number)
+{	
+	
+	return number * number;
+}
 
 
-    std::srand(static_cast<unsigned int>(std::time(nullptr)));
 
-    
-    int index = std::rand() % 3;
-
-  
-    std::cout << haikus[index] << std::endl;
-
-    return 0;
+int main(int argc, char **argv)
+{
+	printf("Test 123\n");
+	return 0;
 }
