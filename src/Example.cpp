8fa293959e0d0bfdd104d9ba0c3eaf636e46a3ca
Sam Kasimalla <45541473+skasimalla@users.noreply.github.com>
2024-04-26 16:15:51 -0400

    Update Example.cpp

--- src/Example.cpp
+++ src/Example.cpp
@@ -1,17 +1,32 @@
-#include<stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
-using namespace std;
+#include <zlib.h>
 
-int square(int number)
-{	
-	
-	return number * number;
-}
+int main(void) {
+    char buffer_in [256] = {"Conan is a MIT-licensed, Open Source package manager for C and C++ development "
+                            "for C and C++ development, allowing development teams to easily and efficiently "
+                            "manage their packages and dependencies across platforms and build systems."};
+    char buffer_out [256] = {0};
 
+    z_stream defstream;
+    defstream.zalloc = Z_NULL;
+    defstream.zfree = Z_NULL;
+    defstream.opaque = Z_NULL;
+    defstream.avail_in = (uInt) strlen(buffer_in);
+    defstream.next_in = (Bytef *) buffer_in;
+    defstream.avail_out = (uInt) sizeof(buffer_out);
+    defstream.next_out = (Bytef *) buffer_out;
 
+    deflateInit(&defstream, Z_BEST_COMPRESSION);
+    deflate(&defstream, Z_FINISH);
+    deflateEnd(&defstream);
 
-int main(int argc, char **argv)
-{
-	printf("Test 123\n");
-	return 0;
+    printf("Uncompressed size is: %lu\n", strlen(buffer_in));
+    printf("Compressed size is: %lu\n", strlen(buffer_out));
+
+    printf("ZLIB VERSION: %s\n", zlibVersion());
+
+    return EXIT_SUCCESS;
 }
