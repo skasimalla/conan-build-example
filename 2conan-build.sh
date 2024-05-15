30c3d3399d0a81ec9e16b93cace3ba5ab194f246
Sam K <skasimalla@gmail.com>
2024-05-15 17:19:38 -0400

    Updates

--- 2conan-build.sh
+++ 2conan-build.sh
@@ -27,11 +27,15 @@ jf rt ping
 
 echo $BUILD_NUMBER
 
-export JFROG_CLI_BUILD_URL=http://google.com
-export JFROG_CLI_BUILD_NAME=theringbuild
-export JFROG_CLI_BUILD_NUMBER=$(date +%s)
+export JFROG_CLI_BUILD_URL=http://mill.jfrog.info:13132/
+export JFROG_CLI_BUILD_NAME=compressor-tc
+export JFROG_CLI_BUILD_NUMBER=$BUILD_NUMBER
 
 pwd
+
+
+jf rt build-add-git samk-generic-local 1.0.${BUILD_NUMBER}
+jf rt build-collect-env samk-generic-local 1.0.${BUILD_NUMBER}
 
 jf rt upload compressor samk-generic-local/compressor-tc/1.0.${BUILD_NUMBER} --build-name=compressor-build --build-number=1.0.${BUILD_NUMBER}
 jf rt build-publish samk-generic-local/compressor-tc 1.0.${BUILD_NUMBER}
