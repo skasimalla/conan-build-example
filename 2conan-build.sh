8241ea33c0bb2d7b9431bd344eddf50798326797
Sam Kasimalla <45541473+skasimalla@users.noreply.github.com>
2024-05-14 22:32:59 -0400

    Create 2conan-build.sh

---
+++ 2conan-build.sh
@@ -0,0 +1,17 @@
+jf c add $SERVER_ID --url=$URL --access-token=$ACCESS_TOKEN --interactive=false
+jf rt ping
+
+sudo apt-get install gcc g++ cmake  
+    
+mkdir build
+conan profile detect --force
+conan install . --output-folder=build --build=missing
+
+cd build
+cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
+cmake --build .
+
+echo 1.0.${{github.run_number}}
+jf rt upload compressor samk-generic-local/compressor/1.0.${{github.run_number}} --build-name=compressor-build --build-number=1.0.${{github.run_number}}
+jf rt build-publish samk-generic-local/compressor 1.0.${{github.run_number}}
+jf rt build-scan samk-generic-local/compressor 1.0.${{github.run_number}}
