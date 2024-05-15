5d5ad5479f040ceb8bdeabc3d51338c46e67b496
Sam Kasimalla <45541473+skasimalla@users.noreply.github.com>
2024-05-14 23:00:19 -0400

    Update 2conan-build.sh

--- 2conan-build.sh
+++ 2conan-build.sh
@@ -13,11 +13,11 @@ cd build
 cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
 cmake --build .
 
-BUILD_NUMBER=1.0.51
+BUILD_NUMBER=51
 
 echo $BUILD_NUMBER
 
 
-jf rt upload compressor samk-generic-local/compressor/1.0.${{BUILD_NUMBER}} --build-name=compressor-build --build-number=1.0.${{BUILD_NUMBER}}
-jf rt build-publish samk-generic-local/compressor 1.0.${{BUILD_NUMBER}}
-jf rt build-scan samk-generic-local/compressor 1.0.${{BUILD_NUMBER}}
+jf rt upload compressor samk-generic-local/compressor/1.0.${BUILD_NUMBER} --build-name=compressor-build --build-number=1.0.${BUILD_NUMBER}
+jf rt build-publish samk-generic-local/compressor 1.0.${BUILD_NUMBER}
+jf rt build-scan samk-generic-local/compressor 1.0.${BUILD_NUMBER}
