3c89d34a3015305610943aabdf18917ee4047754
Sam K <skasimalla@gmail.com>
2024-05-19 02:30:09 -0400

    Updates

--- windows-conan2-build.bat
+++ windows-conan2-build.bat
@@ -1,6 +1,6 @@
 echo "Starting build"
 
-#THis shoulld go in the agent heating script
+REM THis shoulld go in the agent heating script
 REM $env:Path += ';C:\Program Files\Conan\conan'
 REM $env:Path += ';C:\Program Files\CMake\bin'
 
@@ -9,7 +9,7 @@ SET PATH=%PATH%;C:\Program Files\Conan\conan
 SET PATH=%PATH%;C:\Program Files\CMake\bin
 
 
-#apt-get install gcc g++ cmake  
+REM #apt-get install gcc g++ cmake  
     
 rm -rf build
 mkdir -p build
@@ -30,11 +30,11 @@ cmake --build .
 
 #Uploading to RT
 
-echo $URL
-jf c add $SERVER_ID --url=$URL --access-token=$ACCESS_TOKEN --interactive=false
+echo %URL%
+jf c add soleng --url=%SURL% --access-token=%ACCESS_TOKEN% --interactive=false
 jf rt ping
 
-echo $BUILD_NUMBER
+echo %BUILD_NUMBER%
 
 REM export JFROG_CLI_BUILD_URL=http://mill.jfrog.info:13132/
 REM export JFROG_CLI_BUILD_NAME=compressor-tc
@@ -43,10 +43,10 @@ REM export JFROG_CLI_BUILD_NUMBER=$BUILD_NUMBER
 pwd
 
 zip -r compressor.zip . 
-jf rt build-add-git samk-generic-local 1.0.${BUILD_NUMBER}
-jf rt build-collect-env samk-generic-local 1.0.${BUILD_NUMBER}
-jf rt bad compressor-tc 1.0.${BUILD_NUMBER} ./
+jf rt build-add-git samk-generic-local 1.0.%BUILD_NUMBER%
+jf rt build-collect-env samk-generic-local 1.0.%BUILD_NUMBER%
+jf rt bad compressor-tc 1.0.%BUILD_NUMBER% ./
 
-jf rt upload compressor.zip samk-generic-local/compressor-tc/compressor-1.0.${BUILD_NUMBER}.zip --build-name=compressor-tc --build-number=1.0.${BUILD_NUMBER}
-jf rt build-publish samk-generic-local/compressor-tc 1.0.${BUILD_NUMBER}
-jf rt build-scan samk-generic-local/compressor-tc 1.0.${BUILD_NUMBER}
+jf rt upload compressor.zip samk-generic-local/compressor-tc/compressor-1.0.%BUILD_NUMBER%.zip --build-name=compressor-tc --build-number=1.0.%BUILD_NUMBER%
+jf rt build-publish samk-generic-local/compressor-tc 1.0.%BUILD_NUMBER%
+jf rt build-scan samk-generic-local/compressor-tc 1.0.%BUILD_NUMBER%
